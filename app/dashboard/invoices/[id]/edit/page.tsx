96dd351acac8944034a526461776ab5762231d95
Venugopal Pulyala <vpulyala@osius.com>
2024-03-04 19:24:51 +0530

    Handling errors, improving accessibility, adding authentication and metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,12 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices Update',
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -8,7 +14,9 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
-
+  if (!invoice) {
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
